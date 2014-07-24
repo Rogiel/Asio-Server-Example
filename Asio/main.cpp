ba031fbc1a61ef244f514953d89e4a08ec893e38
Rogiel Sulzbach <rogiel@rogiel.com>
2014-07-24 11:57:07 -0300

    Initial Commit

---
+++ Asio/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Asio
+//
+//  Created by Rogiel Sulzbach on 7/24/14.
+//  Copyright (c) 2014 Rogiel Sulzbach. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
